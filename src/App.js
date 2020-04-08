afed64f664f174d2ffe5575e438d2cf6c04c968b
blenky36 <philblenk@hotmail.co.uk>
2020-04-08 14:44:15 +0100

    playing multiple sounds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import wood from './audio/wood.wav';
+import drumstick from './audio/drumstick.wav';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [seconds, setSeconds] = useState(0);
+    const [playing, setPlaying] = useState(false);
+    const [intervalID, setIntervalID] = useState(0);
+    const [tickSpeed, setTickSpeed] = useState(500);
+
+    const startMetronome = () => {
+        let t = new Audio(wood);
+        setPlaying(true);
+        playSound(t);
+        setIntervalID(setInterval(() => {
+            playSound(t);
+            setSeconds(seconds => seconds + 1);
+        }, tickSpeed));
+    }
+
+    const stopMetronome = () => {
+        clearInterval(intervalID);
+        setPlaying(false);
+    }
+
+    const getBeatsPerMinute = () => 60 / (tickSpeed / 1000)
+    const playSound = (t) => {
+        let clone = t.cloneNode();
+        clone.play();
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {seconds} seconds have elapsed since mounting.
+                <br/>
+                {getBeatsPerMinute()} BPM
+                {playing ? <button className="btn btn-danger" onClick={() => stopMetronome()}>Stop</button> : <button disabled={playing} className="btn btn-success" onClick={() => startMetronome('drumstick')}>Play</button>}
+                
+                {/* <audio id={'wood'} src={wood} type="audio/wav" ></audio> */}
+                {/* <audio id={'drumstick'} src={drumstick} type="audio/wav" ></audio> */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
